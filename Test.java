0d5d9b13a1dc3c0474399f5ed112d45fb91a46e7
DerrickCarboncaptureNstorage <derrickodondiri@gmail.com>
2024-08-29 14:20:26 +0000

    newly updated

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
 Testing by quality assurance team
 Files are testing web apps befor deployment
   runn unit test cases
+Derrick has money
+ money is here
---
+++ cat Test.java
@@ -0,0 +1,59 @@
+[33mcommit 522085572bbb14eeef6fdbe6b34d0a11dc19720b[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m, [m[1;31mderrick/master[m[33m)[m
+Author: DerrickCarboncaptureNstorage <92622544+DerrickCarboncaptureNstorage@users.noreply.github.com>
+Date:   Thu Aug 29 05:34:50 2024 +0100
+
+    Update Test.java
+
+[33mcommit e6ee55859ccf97243ba8e3659032d2f998cd5a17[m
+Author: DerrickCarboncaptureNstorage <92622544+DerrickCarboncaptureNstorage@users.noreply.github.com>
+Date:   Thu Aug 29 05:30:10 2024 +0100
+
+    Create Test.java
+
+[33mcommit 4f2d69cd390e0d2be1c572053e3e835219f45c73[m
+Author: Darian <derrickodondiri@gmail.com>
+Date:   Thu Aug 29 05:19:34 2024 +0100
+
+     Tari added
+
+[33mcommit 700dcdce917c2abc2db316a1cc730981c561b9b8[m
+Author: Darian <derrickodondiri@gmail.com>
+Date:   Thu Aug 29 05:04:59 2024 +0100
+
+     list modified
+
+[33mcommit fe165fd47402b3aac8b2e038a83cf67482c0ac00[m
+Author: Darian <derrickodondiri@gmail.com>
+Date:   Thu Aug 29 04:51:09 2024 +0100
+
+    deploy.sh created
+
+[33mcommit 2d219b7925b38f05c92aa4790c25cd2f07902ac5[m
+Author: DerrickCarboncaptureNstorage <92622544+DerrickCarboncaptureNstorage@users.noreply.github.com>
+Date:   Thu Aug 29 04:28:10 2024 +0100
+
+    Update README.md
+
+[33mcommit 36b93e92c2f63b2bfb09a0c6f765aca7ab824378[m
+Author: DerrickCarboncaptureNstorage <92622544+DerrickCarboncaptureNstorage@users.noreply.github.com>
+Date:   Thu Aug 29 04:27:52 2024 +0100
+
+    Update README.md
+
+[33mcommit 7c49b0d8ad138841499e206a2f258b13db6b1736[m
+Author: DerrickCarboncaptureNstorage <derrickodondiri@gmail.com>
+Date:   Thu Aug 29 03:21:32 2024 +0000
+
+    deployemt and prayer added
+
+[33mcommit e1b4c61e6bbc869ae5c05efdfce392f358aca599[m
+Author: DerrickCarboncaptureNstorage <derrickodondiri@gmail.com>
+Date:   Thu Aug 29 02:21:08 2024 +0000
+
+    more billinaires and billion
+
+[33mcommit e5dc9ec6b9c835bc45ff68bcbe144a78eb81e09d[m
+Author: DerrickCarboncaptureNstorage <derrickodondiri@gmail.com>
+Date:   Thu Aug 29 01:59:40 2024 +0000
+
+    first commit
